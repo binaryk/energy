93da52429350c5c512d577764a81919b115de45d
binaryk <lupacescueduard@yahoo.com>
2016-01-10 00:09:35 +0200

    Landing page

--- resources/views/~layouts/template/parts/body/~breadcrumbs.blade.php
+++ resources/views/~layouts/template/parts/body/~breadcrumbs.blade.php
@@ -1,20 +1,21 @@
-<ol class="breadcrumb">
-	<li>
-		<a href="{{URL::to('/') }}">
-			<i class="icon-home"></i>
-			Acasă
-		</a>
-
-	</li>
-
 @if( isset($breadcrumbs) )
-	@foreach($breadcrumbs as $key => $breadcrumb)
+	<ol class="breadcrumb">
 		<li>
-			<a href="{!! route($breadcrumb['route'], $breadcrumb['ids']) !!}">{!! $breadcrumb['name'] !!}</a>
-			@if( $key < count($breadcrumbs) - 1 )
-				<i class="fa fa-angle-right"></i>
-			@endif
+			<a href="{{URL::to('/') }}">
+				<i class="icon-home"></i>
+				Acasă
+			</a>
+
 		</li>
-	@endforeach
-	@endif
-</ol>
+
+
+		@foreach($breadcrumbs as $key => $breadcrumb)
+			<li>
+				<a href="{!! route($breadcrumb['route'], $breadcrumb['ids']) !!}">{!! $breadcrumb['name'] !!}</a>
+				@if( $key < count($breadcrumbs) - 1 )
+					<i class="fa fa-angle-right"></i>
+				@endif
+			</li>
+		@endforeach
+	</ol>
+@endif
