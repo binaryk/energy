58b25cd3d7181d0adc6eba6fd3e88af66236ad45
Vadik <borodinvadim0206@gmail.com>
2016-01-10 01:21:43 +0200

    dashbord_test

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,44 +2,51 @@
 @section('content')
     <div class="m-t-n m-b">
         <div class="card m-b-0 bg-primary-dark p-a-md no-border m-b m-x-n-g">
-            <div class="card-block" style="height: 200px">
+            <div class="card-block" style="height: 80px">
             </div>
         </div>
         <div class="row post-header text-white">
-            <div class="col p-b-lg col-xs-8 col-xs-offset-2">
-                <h1>Despre aplicatie</h1>
-                <h4>Referinte</h4>
-                <div class="meta">
-                    <span class="author">by <span class="author-name">Sean Carpenter</span></span>
-                    <span class="pub-date">Posted Dec. 25, 2015</span>
+            <div class="col p-b-lg col-xs-10 col-xs-offset-2">
+                <h2>INDICATORI DE EFICIENTA ENERGETICA pentru ROMANIA</h2>
+                <h4 align="center">Referinte: <a href="http://www.worldenergy.org/data/efficiency-indicators/" target="_blanck">http://www.worldenergy.org/data/efficiency-indicators/</a></h4>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="p-b-lg col-sm-10 col-sm-offset-1">
+                <article class="article">
+                <div class="col-md-6">
+                    <p>&nbsp;&nbsp;Eficienţa energetică este un termen foarte larg care se referă la multele modalităţi prin care putem obţine acelaşi beneficiu (lumină, încălzire, mişcare, etc.) folosind mai puţină energie. Domeniul acoperă automobilele eficiente, becurile economice, practicile industriale îmbunătăţite, izolarea mai bună a caselor şi o gamă de alte tehnologii. Pentru că economisirea energiei înseamnă şi economisirea banilor, eficienţa energetică este foarte profitabilă.</p>
+                     <h4>Mai bun cu mai puțin</h4>
+                     <p>Eficienţa energetică are adesea multiple efecte benefice. De exemplu, o maşină de spălat rufe eficientă sau o maşină de spălat vase foloseşte mai puţină apă. Eficienţa furnizează de obicei şi un nivel mai înalt de confort. De exemplu, o casă bine izolată va fi mai răcoroasă vara şi mai călduroasă iarna. 
+                     Un frigider eficient va fi mai silenţios, nu va îngheţa sau face condens şi în mod sigur va avea o durată mai mare de funcţionare. Iluminatul eficient îţi va oferi mai multă lumină acolo unde ai nevoie. Astfel, eficienţa este de fapt „mai bine cu mai puţin”.. Eficienţa are un potenţial enorm. Sunt nişte paşi foarte simpli pe care îi poţi urma, cum ar fi să izolezi mai bine acoperişul, sau cumperi o maşină de spălat rufe mai eficientă atunci când se strică cea veche. 
+                     Toate aceste exemple îţi vor aduce atât economii de bani cât şi de energie. Însă cele mai mari economii nu se obţin dacă faci doar un pas din zece. Adevăratele câştiguri vin din regândirea întregului concept, adică „a întregii case”, de exemplu, sau „a întregului sistem de transport”. Când vei realiza acest lucru, în mod surprinzător, consumul de energie poate fi redus de 4, până la 10 ori din nivelul actual.   </p>
                 </div>
+                    <figure class="cap-left">
+                        <img src="{!! asset('images/CE.jpg') !!}" alt="" class="img-responsive">
+                        <figcaption>GreenPeace.</figcaption>
+                    </figure>
+                </article>
             </div>
         </div>
         <div class="row">
-            <div class="p-b-lg col-sm-8 col-sm-offset-2">
+            <div class="p-b-lg col-sm-10 col-sm-offset-1">
                 <article class="article">
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-                    <p><sup>[1]</sup>Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                    <p>Donec id elit non mi porta gravida at eget metus. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui. Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vestibulum id ligula porta felis euismod semper.Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-                    <figure class="cap-left">
-                        <img src="http://lorempixel.com/800/600?1" alt="" class="img-responsive">
-                        <figcaption>Vivamus sagittis dolor auctor.</figcaption>
-                    </figure>
-                    <h4>Venenatis</h4>
-                    <p>Curabitur blandit tempus porttitor. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Donec sed odio dui. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur blandit tempus porttitor.</p>
-                    <h4>Nullam id dolor</h4>
-                    <h5>vestibulum</h5>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas faucibus mollis interdum. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor.</p>
-                    <h5>Nascetur ridiculus</h5>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                <div class="col-md-6">
                     <figure class="cap-right">
-                        <img src="http://lorempixel.com/800/600?2" alt="" class="img-responsive">
-                        <figcaption>Vivamus sagittis dolor auctor.</figcaption>
+                        <img src="{!! asset('images/imagine.jpg') !!}" alt="" class="img-responsive">
+                        <figcaption>GreenPeace.</figcaption>
                     </figure>
-                    <p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                </div>
+                    <h4>Mergem mai departe</h4>
+                    <p>Dacă economisirea energiei are un impact economic atât de mare, de ce nu face toată lumea acest lucru? Pentru început, mulţi oameni profită de eficienţa energetică. Potrivit consiliului American pentru o Economie Eficientă Energetic (American council for an Energy-Efficient Economy) „Consumul total primar de energie pe persoană, în Statele Unite, în anul 2000 a fost aproape identic cu cel din 1973. De-a lungul aceleiaşi perioade de 27 de ani, producţia economică (PIB-ul, sau GDP-ul) pe locuitor a crescut cu 74 la sută.”
+                    Dar asta este numai începutul. Pentru a valorifica cu adevărat potenţialul eficienţei energetice, ceea ce, din păcate, este mai dificil decât să continuăm exploatarea de petrol, avem nevoie de politici guvernamentale corespunzătoare. Astfel, cel mai important lucru este stabilirea unui standard minim de eficienţă energetică pentru case, birouri, maşini, electronice, electrocasnice etc., care să reflecte consumul pe perioada minimă de funcţionare. Consumatorii au dreptul să presupună că produsele cumpărate respectă un anumit standard minim, cum, de exemplu, există deja standardele minime de siguranţă. Totuşi standardele pentru eficienţă electrică sunt prea des neglijate de către guverne sau sunt prea permisive. Guvernele ar trebui de asemenea să implementeze politici care să promoveze  inovaţia şi îmbunătăţirea permanentă a tehnologiilor eficiente energetic.
+                    Pentru mai multe exemple de eficienţă energetică și află cum poţi reduce propriul consum de energie electric, de patru, până la de zece ori.  </p>
+                    <h4>Electronice mai verzi</h4>
+                    <p>Lumea consumă anual din ce în ce mai multe produse electronice. Acest fapt a provocat o creştere periculoasă a deşeurilor electronice ce conţin substanţe chimice toxice şi metale grele ce nu pot fi eliminate sau reciclate în siguranţă. Dar această problemă poate fi evitată.Vrem să determinăm liderii de piaţă producători de electronice să elimine fluxul toxic de deşeuri electronice.Expunem companiile de electronice presiunii publicului, ajutând astfel la crearea unei industrii mai „verzi”. Noi ştim că aceste companii ar putea face mult mai mult, să nu se ocupe doar de ce lasă în urmă, ci şi să ajute la prevenirea consecinţelor schimbărilor climatice.De aceea Greenpeace a demarat campania Provocarea Liderilor din Climatul IT. Ţinta este, în principal, determinarea executivilor din companiile de IT să sprijine activităţi de lobby pe lângă guverne în favoarea unui acord puternic.</p>
                 </article>
             </div>
         </div>
     </div>
-    </div>
+     
 @stop
